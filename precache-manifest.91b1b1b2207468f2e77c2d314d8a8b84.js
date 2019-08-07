9a5a03f5ff60056dac80855cd29bdcfd367efbbc
khanh <khanhng3009@gmail.com>
2019-08-07 12:38:47 -0700

    Updates

---
+++ precache-manifest.91b1b1b2207468f2e77c2d314d8a8b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5797b7e9d0f367f0230965c2561156ea",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "456e04bddf1a785a819b",
+    "url": "/test/static/css/main.45a8df36.chunk.css"
+  },
+  {
+    "revision": "36ff7d25961a77b910c6",
+    "url": "/test/static/js/2.fedb0732.chunk.js"
+  },
+  {
+    "revision": "456e04bddf1a785a819b",
+    "url": "/test/static/js/main.bd326e5b.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
